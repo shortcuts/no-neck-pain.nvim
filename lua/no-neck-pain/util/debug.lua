609cf06009cda873d0bd378fd2768e4bcf18598d
shortcuts <vannicattec@gmail.com>
2023-01-01 17:00:42 +0100

    remove log

--- lua/no-neck-pain/util/debug.lua
+++ lua/no-neck-pain/util/debug.lua
@@ -26,9 +26,9 @@ end
 
 -- prints table only if debug is true.
 function D.tprint(table, indent)
-    -- if _G.NoNeckPain.config ~= nil and not _G.NoNeckPain.config.debug then
-    --     return
-    -- end
+    if _G.NoNeckPain.config ~= nil and not _G.NoNeckPain.config.debug then
+        return
+    end
 
     if not indent then
         indent = 0
