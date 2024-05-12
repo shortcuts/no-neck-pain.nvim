a477d7bd9c103169bec79994e7028f9d8f84e97b
shortcuts <vannicattec@gmail.com>
2024-05-12 23:45:25 +0200

    fix(autocmds): do not force close unsaved buffers

--- tests/test_API.lua
+++ tests/test_API.lua
@@ -358,4 +358,26 @@ T["disable"]["(multiple tab) resets state"] = function()
     Helpers.expect.state(child, "tabs", vim.NIL)
 end
 
+T["disable"]["does not close the window if unsaved buffer"] = function()
+    child.set_size(500, 500)
+    child.lua([[ require('no-neck-pain').setup({width=50}) ]])
+    Helpers.toggle(child)
+
+    Helpers.expect.state(child, "enabled", true)
+
+    Helpers.expect.state(child, "tabs[1].wins.main", {
+        curr = 1000,
+        left = 1001,
+        right = 1002,
+    })
+    Helpers.expect.equality(Helpers.listBuffers(child), { 1, 2, 3 })
+
+    child.api.nvim_buf_set_lines(1, 0, 1, false, { "foo" })
+    child.cmd("quit")
+
+    Helpers.expect.equality(Helpers.listBuffers(child), { 2 })
+    Helpers.expect.state(child, "enabled", false)
+    Helpers.expect.equality(Helpers.listBuffers(child), { 1 })
+end
+
 return T
