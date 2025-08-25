03e17e34968e68f59f5225c8041a70e5fb32f873
shortcuts <vannicattec@gmail.com>
2025-08-25 23:12:24 +0200

    chore: add more local scripts and fix scratch_pad casing

---
+++ scripts/init_only_right_side.lua
@@ -0,0 +1,16 @@
+vim.cmd([[let &rtp.=','.getcwd()]])
+
+vim.cmd("set rtp+=deps/mini.nvim")
+
+vim.g.mapleader = ";"
+
+-- Auto open enabled for the test
+require("no-neck-pain").setup({
+    debug = true,
+    width = 50,
+    minSideBufferWidth = 5,
+    autocmds = { enableOnVimEnter = true, enableOnTabEnter = true },
+    mappings = { enabled = true },
+    buffers = { left = { enabled = false } },
+})
+require("mini.test").setup()
